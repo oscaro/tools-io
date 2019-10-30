8b3f5198777eb498df36976f898285a5bd4da0b2
NiKo <nicolas.monville@oscaro.com>
2019-10-30 17:44:00 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/oscaro/tools-io"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.8.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.9.0"]
                  [circleci/clj-yaml "0.6.0"]
                  [org.clojure/data.csv "0.1.4"]]
   ;; https://www.blog.nodrama.io/travis-continuous-delivery/
