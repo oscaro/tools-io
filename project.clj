f5e095251d82d012a068814952675535d7609a6f
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2019-11-21 17:20:49 +0100

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io "0.3.18"
+(defproject com.oscaro/tools-io "0.3.19-SNAPSHOT"
   :description "Oscaro's generic I/O tools collection"
   :url "https://github.com/oscaro/tools-io"
   :license {:name "Eclipse Public License"
