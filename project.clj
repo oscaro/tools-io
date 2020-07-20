240519a02d28ca7bbc3f88e727d2c0d91c2bc106
Jérôme Prudent <jerome.prudent.ext@oscaro.com>
2020-07-20 16:02:59 +0200

    Bump cheshire to 5.10.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cheshire "5.9.0"]
+                 [cheshire "5.10.0"]
                  [circleci/clj-yaml "0.6.0"]
                  [org.clojure/data.csv "0.1.4"]]
   ;; https://www.blog.nodrama.io/travis-continuous-delivery/
