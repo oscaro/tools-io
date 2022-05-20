33342cc221ec3ebe07df3d211b84b441e89afeef
Ed Porras <dev@digressed.net>
2022-05-20 15:14:53 +0200

    fix: disable slash-escaping to maintain original behavior pre-charred

--- test/tools/io_test.clj
+++ test/tools/io_test.clj
@@ -9,18 +9,18 @@
 (deftest write-jsons-file-test
   (let [data [{:a 1 :b 2.3 :c "c1/to" :d [1 2 3] :e #{4 2 45}}
               {:a 2 :b 12.3 :c "c2/\"ta" :d [45 5] :f #{1}}]
-        expected "{\"a\":1,\"b\":2.3,\"c\":\"c1\\/to\",\"d\":[1,2,3],\"e\":[4,2,45]}\n{\"a\":2,\"b\":12.3,\"c\":\"c2\\/\\\"ta\",\"d\":[45,5],\"f\":[1]}\n"]
-    (testing "nominal case"
-      (sut/with-tempdir [tmp-dir]
+        expected "{\"a\":1,\"b\":2.3,\"c\":\"c1/to\",\"d\":[1,2,3],\"e\":[4,2,45]}\n{\"a\":2,\"b\":12.3,\"c\":\"c2/\\\"ta\",\"d\":[45,5],\"f\":[1]}\n"]
+
+    (sut/with-tempdir [tmp-dir]
+      (testing "nominal case"
         (let [file-path (sut/join-path tmp-dir "test.jsons")]
           (sut/write-jsons-file file-path data)
 
           (let [rslt (slurp file-path)]
             (is (= expected
-                   rslt))))))
+                   rslt)))))
 
-    (testing "with compression"
-      (sut/with-tempdir [tmp-dir]
+      (testing "with compression"
         (let [file-path (sut/join-path tmp-dir "test.jsons.gz")]
           (sut/write-jsons-file file-path data)
 
