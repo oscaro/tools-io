fc10b3496a4acfd002b6afd4d8b670bb4de53086
Ed Porras <dev@digressed.net>
2022-12-02 13:31:41 +0100

    deps: bump charred 1.016, clj-yaml 1.0.26

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
 
-                 [clj-commons/clj-yaml "0.7.108"]
-                 [com.cnuernber/charred "1.011"]]
+                 [clj-commons/clj-yaml "1.0.26"]
+                 [com.cnuernber/charred "1.016"]]
   ;; https://www.blog.nodrama.io/travis-continuous-delivery/
   ;; https://github.com/technomancy/leiningen/issues/2307#issuecomment-610538144
   :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
