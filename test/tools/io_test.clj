15b785693cc69e3857ae0250530dd98dbd42d2d9
Ed Porras <eduardo.porras@oscaro.com>
2023-03-16 09:59:07 +0100

    test: add read-edns / write-edn / additional read-jsons-file test

--- test/tools/io_test.clj
+++ test/tools/io_test.clj
@@ -21,15 +21,24 @@
 
     (sut/with-tempdir [tmp-dir]
       (testing "nominal case"
-        (let [file-path (sut/join-path tmp-dir "test.jsons")]
-          (write-fixture file-path data)
+        (testing "with collection"
+          (let [file-path (sut/join-path tmp-dir "test-coll.jsons")]
+            (write-fixture file-path data)
 
-          (let [rslt (sut/read-jsons-file file-path)]
-            (is (= expected
-                   rslt)))))
+            (let [rslt (sut/read-jsons-file file-path)]
+              (is (= expected
+                     rslt)))))
+
+        (testing "idents"
+          (let [data "1\n2\n3"
+                file-path (sut/join-path tmp-dir "test-ident.jsons")]
+            (write-fixture file-path data)
+            (let [rslt (sut/read-jsons-file file-path)]
+              (is (= [1 2 3]
+                     rslt))))))
 
       (testing "with compression"
-        (let [file-path (sut/join-path tmp-dir "test.jsons.gz")]
+        (let [file-path (sut/join-path tmp-dir "test-comp.jsons.gz")]
           (write-fixture file-path data)
 
           (let [rslt (sut/read-jsons-file file-path)]
@@ -53,6 +62,54 @@
       (testing "with compression"
         (let [file-path (sut/join-path tmp-dir "test.jsons.gz")]
           (sut/write-jsons-file file-path data)
+
+          (let [rslt (->> file-path
+                          (io/input-stream)
+                          (GZIPInputStream.)
+                          slurp)]
+            (is (= expected
+                   rslt))))))))
+
+(deftest read-edns-file-test
+  (let [expected [{:a 1 :b 2.3 :c "c1/to" :d [1 2 3] :e [4 2 45]}
+                  {:a 2 :b 12.3 :c "c2/\"ta" :d [45 5] :f [1]}]
+        data "{:a 1, :b 2.3, :c \"c1/to\", :d [1,2,3], :e [4,2,45]}\n{:a 2, :b 12.3, :c \"c2/\\\"ta\", :d [45,5], :f [1]}\n"]
+
+    (sut/with-tempdir [tmp-dir]
+      (testing "nominal case"
+        (testing "with collection"
+          (let [file-path (sut/join-path tmp-dir "test-coll.edns")]
+            (write-fixture file-path data)
+
+            (let [rslt (sut/read-edns-file file-path)]
+              (is (= expected
+                     rslt))))))
+
+      (testing "with compression"
+          (let [file-path (sut/join-path tmp-dir "test-comp.edns.gz")]
+            (write-fixture file-path data)
+
+            (let [rslt (sut/read-edns-file file-path)]
+              (is (= expected
+                     rslt))))))))
+
+(deftest write-edn-file-test
+  (let [data [[1 2 3 4 "abc" 434.23]
+              [10 20 30 40 "edf" 1432.23123]]
+        expected "[[[1 2 3 4 \"abc\" 434.23] [10 20 30 40 \"edf\" 1432.23123]]]\n"]
+
+    (sut/with-tempdir [tmp-dir]
+      (testing "nominal case"
+        (let [file-path (sut/join-path tmp-dir "test.edn")]
+          (sut/write-edn-file file-path data)
+
+          (let [rslt (slurp file-path)]
+            (is (= expected
+                   rslt)))))
+
+      (testing "with compression"
+        (let [file-path (sut/join-path tmp-dir "test.csvs.gz")]
+          (sut/write-edn-file file-path data)
 
           (let [rslt (->> file-path
                           (io/input-stream)
