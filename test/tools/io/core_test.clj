6b6699427fa0e74653df390818ec3ea74e4e3076
Ed Porras <eduardo.porras@oscaro.com>
2023-03-16 13:24:15 +0100

    test: relocate core tests to io_test to match src paths

--- test/tools/io/core_test.clj
+++ test/tools/io/core_test.clj
@@ -1,73 +1,12 @@
 (ns tools.io.core-test
   (:require
    [charred.api :as charred]
-   [clojure.edn :as edn]
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.test :refer [are deftest is testing]]
    [tools.io :as tio]
    [tools.io.core :as sut])
   (:import (java.io File)))
-
-
-(deftest strip-bom-test
-  (testing "bom"
-    (is (= "foo" (tio/strip-bom "\uFEFFfoo"))))
-  (testing "no bom"
-    (is (= "foo" (tio/strip-bom "foo")))
-    (is (= "" (tio/strip-bom "")))))
-
-(deftest join-path-test
-  (is (= "foo/bar" (tio/join-path "foo/bar")))
-  (is (= "foo/bar" (tio/join-path "foo" "bar")))
-  (is (= "foo/bar" (tio/join-path "foo/" "bar")))
-  (is (= "foo/bar" (tio/join-path "foo" "/bar")))
-  (is (= "foo/bar" (tio/join-path "foo/" "/bar")))
-  (is (= "foo/bar" (tio/join-path "foo//" "bar")))
-  (is (= "/foo/bar" (tio/join-path "/foo/" "bar")))
-  (is (= "gs://bar/a/b/c" (tio/join-path "gs://bar" "a" "b" "c")))
-
-  (is (= "a/../b" (tio/join-path "a" ".." "b")))
-
-  (is (thrown? AssertionError (tio/join-path nil)))
-  (is (thrown? AssertionError (tio/join-path "gs://" "foo" nil "bar"))))
-
-(deftest basename-test
-  (are [expected fullpath] (= expected (tio/basename fullpath))
-    ""    "/"
-    ""    "///////"
-    "foo" "foo"
-    "foo" "foo/"
-    "bar" "foo/bar"
-    "bar" "foo/bar/"
-    "yo"  "http://www.example.com/yo"
-    "ya"  "gs://foo-bar-uqx/1/2/ya"
-    "hey.gz" "/hey.gz"))
-
-(deftest parent-test
-  (are [expected path] (= expected (tio/parent path))
-       nil ""
-       nil "/"
-       "/" "/foo"
-       "/" "/foo/"
-       "/foo" "/foo/bar"
-       "foo" "foo/bar"
-       "/a/b/c/d" "/a/b/c/d/foo.edns"
-       "gs://foo/bar" "gs://foo/bar/qux"
-       "gs://foo/bar" "gs://foo/bar/qux/"))
-
-(deftest splitext-test
-  (are [expected fullpath] (= expected (tio/splitext fullpath))
-    ["" ""]        ""
-    ["/" ""]       "/"
-    [".bashrc" ""] ".bashrc"
-    ["foo" ""]     "foo"
-    ["a.b/c" ""]   "a.b/c"
-
-    ["foo" "tar"]    "foo.tar"
-    ["foo.tar" "gz"] "foo.tar.gz"
-
-    ["http://www.google.com/index" "html"] "http://www.google.com/index.html"))
 
 (deftest expand-home-test
   (let [home (System/getProperty "user.home")]
@@ -76,46 +15,6 @@
          home "~"
          home home
          (str home "/foo/bar") "~/foo/bar")))
-
-(deftest csv-test
-  (testing "CSV file manipulation"
-    (let [f (java.io.File/createTempFile "tool-io-tests" ".csv")
-          data [["abc" "def"]
-                ["ghi" "jkl"]]]
-      (tio/write-csv-file f data)
-      (is (= "abc,def\nghi,jkl\n" (slurp f))
-          "data should be dumped in CSV format")
-      (is (= data (tio/read-csv-file f))
-          "same data should be read from CSV file")
-      (.delete f))))
-
-(deftest read-jsons-test
-  (testing "reading resource json.gz file"
-    (is (= 4 (count (tio/read-jsons-file (io/resource "good.jsons.gz"))))))
-  (testing "reading resource json file with empty lines"
-    (is (= 4 (count (tio/read-jsons-file (io/resource "good-with-empty-lines.jsons"))))))
-  (testing "reading json file with empty lines"
-    (is (= 4 (count (tio/read-jsons-file (str (io/resource "good-with-empty-lines.jsons")))))))
-  (testing "reading json file with BOM"
-    (is (= 1 (count (tio/read-jsons-file (str (io/resource "with-bom.jsons")))))))
-  (testing "reading resource json files"
-    (is (= 8 (count (tio/read-jsons-files [(io/resource "good-with-empty-lines.jsons")
-                                           (io/resource "good.jsons.gz")]))))))
-
-
-(defn- custom-edn-read [s]
-  (edn/read-string {:readers {'double #(* % 2)}} s))
-
-(deftest load-config-files-test
-  (testing "reading resource test.edn file"
-    (is (nil? (tio/load-config-file "not-exists.edn")))
-    (is (thrown? AssertionError (tio/load-config-file "what-s-in-the-box?")) "unknown parser")
-    (is (= 42 (:answer (tio/load-config-file (io/resource "test.edn")))) "load resource")
-    (is (= 84 (:answer (tio/load-config-file (io/resource "test-with-reader-tag.edn") custom-edn-read))) "load resource with custom parser")
-    (is (= 42 (:answer (tio/load-config-file "test.yml"))) "load filename as resource")
-    (is (= 42 (:answer (tio/load-config-file "test/resources/test.json"))) "load absolute filename")
-    (with-in-str "{:answer 42}"
-     (is (= 42 (:answer (tio/load-config-file *in* edn/read-string))) "load from stdin (protocol independant)"))))
 
 (deftest read-txt-test
   (testing "reading resource test.txt file"
@@ -129,42 +28,6 @@
       (tio/write-text-file filename text)
       (is (= text (tio/read-text-file filename)))
       (is (= (str/join "" (map #(str % "\n") text)) (tio/slurp filename)))
-      (finally
-        (io/delete-file filename true)))))
-
-(deftest spit-slurp-test
-  (let [text "Hey\n\nI \njust\n\n  met \n\tyou\n\n"
-        filename ".spit-slurp-tmp-test-rw-text"]
-    (io/delete-file filename true)
-    (try
-      (tio/spit filename text)
-      (is (= text (tio/slurp filename)))
-      (finally
-        (io/delete-file filename true)))))
-
-(deftest line-write-test
-  (let [filename ".line-write-tmp-test-rw-text"]
-    (io/delete-file filename true)
-    (try
-      (let [f (sut/file-writer filename)]
-        (tio/line-write f "foo")
-        (tio/line-write f "bar\n")
-        (tio/line-write f "qux")
-        (sut/close! f))
-      (is (= ["foo" "bar" "qux"]
-             (tio/read-text-file filename)))
-      (finally
-        (io/delete-file filename true)))))
-
-(deftest lines-write-test
-  (let [text ["We're" "no" "strangers" "to" "love"]
-        filename ".lines-write-tmp-test-rw-text"]
-    (io/delete-file filename true)
-    (try
-      (let [f (sut/file-writer filename)]
-        (tio/lines-write f text))
-      (is (= text
-             (tio/read-text-file filename)))
       (finally
         (io/delete-file filename true)))))
 
@@ -280,35 +143,6 @@
         (testing "trailing slash no dir"
           (is (empty? (tio/list-files (tio/join-path dirname "foo/"))))
           (is (empty? (tio/list-files (tio/join-path dirname "aaa/y/")))))))))
-
-(deftest list-dirs-test
-  (testing "list dirs from existant dir"
-    (tio/with-tempdir [dirname]
-      (let [files (->> ["foo"
-                        "bar/x"
-                        "quux/x"
-                        "baz"]
-                       (map #(format "%s/%s" dirname %)))]
-        (doseq [path files]
-          (io/make-parents path)
-          (spit path ""))
-        (is (= 2 (count (tio/list-dirs dirname)))))))
-  (testing "list dirs from existant without subfolders"
-    (is (= 0 (count (tio/list-dirs "test/resources/test")))))
-  (testing "list dirs from existant with trailing slash + subfolders"
-    (tio/with-tempdir [dirname]
-      (let [files (->> ["i"
-                        "love/x/hh"
-                        "testing/x/hey"
-                        "clojure"
-                        "code/itstrue"]
-                       (map #(format "%s/%s/" dirname %)))]
-        (doseq [path files]
-          (io/make-parents path)
-          (spit path ""))
-        (is (= 3 (count (tio/list-dirs dirname)))))))
-  (testing "list dirs from non existant directory"
-    (is (= 0 (count (tio/list-dirs "i'm broken ~~~"))))))
 
 (deftest with-tempfile-test
   (let [*filename* (atom nil)
--- test/tools/io_test.clj
+++ test/tools/io_test.clj
@@ -1,11 +1,127 @@
 (ns tools.io-test
   (:require
+   [clojure.edn :as edn]
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [clojure.test :refer [deftest is testing]]
-   [tools.io :as sut])
+   [clojure.test :refer [are deftest is testing]]
+   [tools.io :as sut]
+   [tools.io.core :refer [file-writer]])
   (:import
-   (java.util.zip GZIPInputStream GZIPOutputStream)))
+   (java.util.zip GZIPInputStream GZIPOutputStream)
+   (org.apache.commons.compress.archivers.zip ZipArchiveEntry ZipFile)))
+
+(deftest strip-bom-test
+  (testing "bom"
+    (is (= "foo" (sut/strip-bom "\uFEFFfoo"))))
+  (testing "no bom"
+    (is (= "foo" (sut/strip-bom "foo")))
+    (is (= "" (sut/strip-bom "")))))
+
+(deftest join-path-test
+  (is (= "foo/bar" (sut/join-path "foo/bar")))
+  (is (= "foo/bar" (sut/join-path "foo" "bar")))
+  (is (= "foo/bar" (sut/join-path "foo/" "bar")))
+  (is (= "foo/bar" (sut/join-path "foo" "/bar")))
+  (is (= "foo/bar" (sut/join-path "foo/" "/bar")))
+  (is (= "foo/bar" (sut/join-path "foo//" "bar")))
+  (is (= "/foo/bar" (sut/join-path "/foo/" "bar")))
+  (is (= "gs://bar/a/b/c" (sut/join-path "gs://bar" "a" "b" "c")))
+
+  (is (= "a/../b" (sut/join-path "a" ".." "b")))
+
+  (is (thrown? AssertionError (sut/join-path nil)))
+  (is (thrown? AssertionError (sut/join-path "gs://" "foo" nil "bar"))))
+
+(deftest basename-test
+  (are [expected fullpath] (= expected (sut/basename fullpath))
+    ""    "/"
+    ""    "///////"
+    "foo" "foo"
+    "foo" "foo/"
+    "bar" "foo/bar"
+    "bar" "foo/bar/"
+    "yo"  "http://www.example.com/yo"
+    "ya"  "gs://foo-bar-uqx/1/2/ya"
+    "hey.gz" "/hey.gz"))
+
+(deftest parent-test
+  (are [expected path] (= expected (sut/parent path))
+       nil ""
+       nil "/"
+       "/" "/foo"
+       "/" "/foo/"
+       "/foo" "/foo/bar"
+       "foo" "foo/bar"
+       "/a/b/c/d" "/a/b/c/d/foo.edns"
+       "gs://foo/bar" "gs://foo/bar/qux"
+       "gs://foo/bar" "gs://foo/bar/qux/"))
+
+(deftest splitext-test
+  (are [expected fullpath] (= expected (sut/splitext fullpath))
+    ["" ""]        ""
+    ["/" ""]       "/"
+    [".bashrc" ""] ".bashrc"
+    ["foo" ""]     "foo"
+    ["a.b/c" ""]   "a.b/c"
+
+    ["foo" "tar"]    "foo.tar"
+    ["foo.tar" "gz"] "foo.tar.gz"
+
+    ["http://www.google.com/index" "html"] "http://www.google.com/index.html"))
+
+(deftest lines-write-test
+  (let [text ["We're" "no" "strangers" "to" "love"]
+        filename ".lines-write-tmp-test-rw-text"]
+    (io/delete-file filename true)
+    (try
+      (let [f (file-writer filename)]
+        (sut/lines-write f text))
+      (is (= text
+             (sut/read-text-file filename)))
+      (finally
+        (io/delete-file filename true)))))
+
+(deftest list-dirs-test
+  (testing "list dirs from existant dir"
+    (sut/with-tempdir [dirname]
+      (let [files (->> ["foo"
+                        "bar/x"
+                        "quux/x"
+                        "baz"]
+                       (map #(format "%s/%s" dirname %)))]
+        (doseq [path files]
+          (io/make-parents path)
+          (spit path ""))
+        (is (= 2 (count (sut/list-dirs dirname)))))))
+
+  (testing "list dirs from existant without subfolders"
+    (is (= 0 (count (sut/list-dirs "test/resources/test")))))
+
+  (testing "list dirs from existant with trailing slash + subfolders"
+    (sut/with-tempdir [dirname]
+      (let [files (->> ["i"
+                        "love/x/hh"
+                        "testing/x/hey"
+                        "clojure"
+                        "code/itstrue"]
+                       (map #(format "%s/%s/" dirname %)))]
+        (doseq [path files]
+          (io/make-parents path)
+          (spit path ""))
+        (is (= 3 (count (sut/list-dirs dirname)))))))
+
+  (testing "list dirs from non existant directory"
+    (is (= 0 (count (sut/list-dirs "i'm broken ~~~"))))))
+
+(deftest spit-test
+  (let [text "Hey\n\nI \njust\n\n  met \n\tyou\n\n"
+        filename ".spit-slurp-tmp-test-rw-text"]
+    (io/delete-file filename true)
+    (try
+      (sut/spit filename text)
+      (is (= text (slurp filename)))
+      (finally
+        (io/delete-file filename true)))))
 
 (defn- write-fixture
   [path data]
@@ -14,7 +130,25 @@
                              GZIPOutputStream.))]
     (.write w ^String data)))
 
+(deftest slurp-test
+  (let [test-file "test.txt"]
+    (is (= (slurp (io/resource test-file))
+           (sut/slurp (io/resource test-file))))))
+
 (deftest read-jsons-file-test
+  (testing "with resources"
+    (testing "reading resource json.gz file"
+      (is (= 4 (count (sut/read-jsons-file (io/resource "good.jsons.gz"))))))
+    (testing "reading resource json file with empty lines"
+      (is (= 4 (count (sut/read-jsons-file (io/resource "good-with-empty-lines.jsons"))))))
+    (testing "reading json file with empty lines"
+      (is (= 4 (count (sut/read-jsons-file (str (io/resource "good-with-empty-lines.jsons")))))))
+    (testing "reading json file with BOM"
+      (is (= 1 (count (sut/read-jsons-file (str (io/resource "with-bom.jsons")))))))
+    (testing "reading resource json files"
+      (is (= 8 (count (sut/read-jsons-files [(io/resource "good-with-empty-lines.jsons")
+                                             (io/resource "good.jsons.gz")]))))))
+
   (let [expected [{:a 1 :b 2.3 :c "c1/to" :d [1 2 3] :e [4 2 45]}
                   {:a 2 :b 12.3 :c "c2/\"ta" :d [45 5] :f [1]}]
         data "{\"a\":1,\"b\":2.3,\"c\":\"c1\\/to\",\"d\":[1,2,3],\"e\":[4,2,45]}\n{\"a\":2,\"b\":12.3,\"c\":\"c2\\/\\\"ta\",\"d\":[45,5],\"f\":[1]}\n"]
@@ -119,9 +253,19 @@
                    rslt))))))))
 
 (deftest read-csv-file-test
+  (testing "CSV file manipulation"
+    (let [f (java.io.File/createTempFile "tool-io-tests" ".csv")
+          data [["abc" "def"]
+                ["ghi" "jkl"]]]
+      (sut/write-csv-file f data)
+      (is (= "abc,def\nghi,jkl\n" (slurp f))
+          "data should be dumped in CSV format")
+      (is (= data (sut/read-csv-file f))
+          "same data should be read from CSV file")
+      (.delete f)))
+
   (let [expected [["1" "2" "3" "4" "abc" "434.23"] ["10" "20" "30" "40" "edf" "1432.23123"]]
         data "1,2,3,4,\"abc\",434.23\n10,20,30,40,\"edf\",1432.23123\n"]
-
     (sut/with-tempdir [tmp-dir]
       (testing "nominal case"
         (let [file-path (sut/join-path tmp-dir "test.csvs")]
@@ -164,3 +308,24 @@
                           slurp)]
             (is (= expected
                    rslt))))))))
+
+(defn- custom-edn-read [s]
+  (edn/read-string {:readers {'double #(* % 2)}} s))
+
+(deftest load-config-files-test
+  (testing "reading resource test.edn file"
+    (is (nil? (sut/load-config-file "not-exists.edn")))
+    (is (thrown? AssertionError (sut/load-config-file "what-s-in-the-box?"))
+        "unknown parser")
+    (is (= 42 (:answer (sut/load-config-file (io/resource "test.edn"))))
+        "load resource")
+    (is (= 84 (:answer (sut/load-config-file (io/resource "test-with-reader-tag.edn") custom-edn-read)))
+        "load resource with custom parser")
+    (is (= 42 (:answer (sut/load-config-file "test.yml")))
+        "load filename as resource")
+    (is (= 42 (:answer (sut/load-config-file "test/resources/test.json")))
+        "load absolute filename")
+
+    (with-in-str "{:answer 42}"
+     (is (= 42 (:answer (sut/load-config-file *in* edn/read-string)))
+         "load from stdin (protocol independant)"))))
