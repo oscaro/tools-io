60ac345dd202c54443025253dd30b11dbc6f5d41
Ed Porras <dev@digressed.net>
2023-04-16 15:43:13 +0200

    Version 0.3.32-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io "0.3.31"
+(defproject com.oscaro/tools-io "0.3.32-SNAPSHOT"
   :description "Oscaro's generic I/O tools collection"
   :url "https://github.com/oscaro/tools-io"
   :license {:name "Eclipse Public License"
