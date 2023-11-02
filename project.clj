251f28151564a13c16a706440875d5f27ad29e39
iomonad <iomonad@riseup.net>
2023-11-02 14:21:00 +0100

    bump: 0.3.34-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io "0.3.33-SNAPSHOT"
+(defproject com.oscaro/tools-io "0.3.34-SNAPSHOT"
   :description "Oscaro's generic I/O tools collection"
   :url "https://github.com/oscaro/tools-io"
   :license {:name "Eclipse Public License"
