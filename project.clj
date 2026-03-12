3637c1dbed598b5fc5b4914b586e9477e1b83ee3
iomonad <iomonad@riseup.net>
2026-03-12 15:57:23 +0100

    feat(deps) bump all deps to latest

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure                 "1.11.4"]
                  [clj-commons/clj-yaml                "1.0.29"]
-                 [com.cnuernber/charred               "1.037"]
+                 [com.cnuernber/charred               "1.038"]
                  [org.apache.commons/commons-compress "1.28.0"]]
   ;; https://www.blog.nodrama.io/travis-continuous-delivery/
   ;; https://github.com/technomancy/leiningen/issues/2307#issuecomment-610538144
@@ -30,10 +30,10 @@
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :source-paths ["dev"]
                    :resource-paths ["test/resources"]
-                   :dependencies [[org.clojure/tools.namespace "1.5.0"]]}
+                   :dependencies [[org.clojure/tools.namespace "1.5.1"]]}
              :extra-compression
-             {:dependencies [[org.tukaani/xz            "1.10"]
-                             [com.github.luben/zstd-jni "1.5.7-6"]]}}
+             {:dependencies [[org.tukaani/xz            "1.12"]
+                             [com.github.luben/zstd-jni "1.5.7-7"]]}}
   :test-selectors {:default (fn [m] (not (:extra-compression m)))
                    :extra-compression :extra-compression}
   :aliases {"repl" ["with-profile" "+extra-compression" "repl"]
